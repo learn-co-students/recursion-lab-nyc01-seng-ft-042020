393e9b724cf7209ba1468d2dd99f4e4df404c966
carolineglass <carolineglass89@gmail.com>
2020-09-14 12:59:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 // Code your solution here!
 
 function printString(myString) {
-    
-    console.log(myString[0]);
-   
     if (myString.length > 1) {
       let mySubString = myString.substring(1, myString.length);
       printString(mySubString);
     } else {
       return true;
+    }
+  }
+
+function reverseString(myString) {
+    if (myString.length < 2) { 
+        return myString; 
+    } else { 
+        return reverseString(myString.substring(1)) + myString[0];
+    }
+}
+
+function includesNumber(myArray, myNumber) {
+    if (!myArray.length) {
+      return false;
+    } else if (myArray[0] === myNumber) {
+      return true;
+    } else {
+      return includesNumber(myArray.slice(1), myNumber);
+    }
+  }
+
+  function maxOf(myArray) {
+    if (myArray.length === 1) {
+      return myArray[0];
+    } else {
+      return Math.max(myArray.pop(), maxOf(myArray));
     }
   }
