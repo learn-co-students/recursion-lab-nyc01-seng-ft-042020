9d21080552ad352ec2a6f613c6f9ea69e10c437f
Jessica Kim <jessikim807@gmail.com>
2020-09-15 11:40:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // Code your solution here!
 
+function printString(myString) {
+    console.log(myString[0]);
+  
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
+}
+  
+function reverseString(myString) {
+    if (myString.length < 2) {
+      return myString;
+    } else {
+      return reverseString(myString.substring(1)) + myString[0];
+    }
+}
